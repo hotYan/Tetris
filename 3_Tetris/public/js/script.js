2dd3611fba0d1156040273c84af943cfcce9784d
hotyan <hotyann@163.com>
2019-08-19 15:27:24 +0800

    课程源码

--- 3_Tetris/public/js/script.js
+++
@@ -1,6 +0,0 @@
-var socket = io();
-var local = new Local(socket);
-var remote = new Remote(socket);
-socket.on('waiting',function(str){
-    document.getElementById('waiting').innerHTML = str;
-});
